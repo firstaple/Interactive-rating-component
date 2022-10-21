a8ab9a6776bf4220b507339646acf352b4198681
firstaple <112137297+firstaple@users.noreply.github.com>
2022-10-21 14:07:18 +0900

    Add files via upload

--- interactive-rating-component-main/main.js
+++ interactive-rating-component-main/main.js
@@ -1,3 +1,14 @@
-function button(button) {
-  document.querySelector("button");
+function button() {
+  const bts = document.querySelectorAll("button");
+  for (let i = 0; i < bts.length; i++) {
+    bts[i].addEventListener(
+      "click",
+      () => {
+        bts[i].style.backgroundColor = "hsl(217, 12%, 63%)";
+        bts[i].style.color = "hsl(0, 0%, 100%)";
+      },
+      { once: true }
+    );
+    console.log(bts[i]);
+  }
 }
